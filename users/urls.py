1303f1a3928f5861028de2d248a49f1df853a403
aliebrahim <alikebrahim@me.com>
2025-04-08 22:04:26 +0300

    Fix /users/* paths

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,20 @@ from django.contrib.auth import views as auth_views
 from .forms import UserLoginForm
 from . import views as v
 
+# Main user URLs
 urlpatterns = [
+    # Registration URL patterns
     path('', v.register, name='register'),
+    path('register/', v.register, name='register'),
+    
+    # Company registration
     path('company/', v.CompanySignUpView.as_view(), name='register_company'),
+    path('register/company/', v.CompanySignUpView.as_view(), name='register_company'),
+    
+    # Customer registration
     path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
+    path('register/customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
+    
+    # Login URL
     path('login/', v.LoginUserView, name='login_user'),
 ]
